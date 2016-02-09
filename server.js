461949c9d31caf9a0dd0c0516bced8857b9cd4a9
Chris Logsdon <logsama@gmail.com>
2016-02-08 23:39:06 -0800

    added get today by id

--- server.js
+++ server.js
@@ -26,7 +26,21 @@ app.get('/todos', function (req, res) {
 
 // get specific todo by id
 app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id,10);
+	var matchedTodo;
 
+	// find a match or error
+	todos.forEach( function(todo) {
+		if (todo.id === todoId) {
+			matchedTodo = todo;
+		}
+	});
+	
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
 });
 
 app.listen(PORT, function () {
