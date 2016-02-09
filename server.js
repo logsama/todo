39beba0d6ee7a91e51753b5f9183d67bd166b21c
Chris Logsdon <logsama@gmail.com>
2016-02-09 11:37:18 -0800

    added underscore

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
 
 app.use(bodyParser.json());
-
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -20,14 +21,7 @@ app.get('/todos', function (req, res) {
 // get specific todo by id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo;
-
-	// find a match or error
-	todos.forEach( function(todo) {
-		if (todo.id === todoId) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos,{id: todoId});
 	
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -36,8 +30,17 @@ app.get('/todos/:id', function (req, res) {
 	}
 });
 
+// add a todo item
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description','completed');
+
+	if (!_.isBoolean(body.completed) ||
+		!_.isString(body.description) ||
+		body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	
+	body.description = body.description.trim();
 
 	body.id = todoNextId++;
 	todos.push(body);
